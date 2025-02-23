acf50f6f04b0c76514dcac276d6e3645b400df95
PhucYasuo <150522312+PhucYasuo@users.noreply.github.com>
2025-02-24 00:54:42 +0700

    init project

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const path = require('path');
+
 const app = express()
 const port = 8080
+
+app.set('views', path.join(__dirname ,'views'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/abc', (req, res) => {
-  res.send('Check abc World!')
+  res.render('sample.ejs')
 })
 
 app.listen(port, () => {
