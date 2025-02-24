6b28273d8c180266d060b1ffe8e5dbd02437c3fe
PhucYasuo <150522312+PhucYasuo@users.noreply.github.com>
2025-02-24 09:26:20 +0700

    init project

--- src/server.js
+++ src/server.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const path = require('path');
+require('dotenv').config();
+
+
 
 const app = express()
-const port = 8080
+const port = process.env.PORT;
+const hostname = process.env.HOST_NAME;
 
+// config template engine
 app.set('views', path.join(__dirname ,'views'))
 app.set('view engine', 'ejs')
 
+// config static files
+app.use(express.static('public'))
+
+// declare route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Phuc')
 })
 
 app.get('/abc', (req, res) => {
   res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+// make sure shit work
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
